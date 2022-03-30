aea37d60cfff6645fb1ac2d7930223cb6aee1bfe
MATEUS VIZOTO <mateus.vizoto@fatec.sp.gov.br>
2022-03-30 10:57:13 -0300

    Aula 30/03

---
+++ AulaPesquisar/Cadastro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar
+{
+    public class Cadastro
+    {
+        public int cpf
+        {
+            get;set;
+        }
+        public int user
+        {
+            get; set;
+        }
+        public int senha
+        {
+            get; set;
+        }
+    }
+}
---
+++ AulaPesquisar/Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPesquisar.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    [HttpPost]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
